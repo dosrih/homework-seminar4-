7188278bc5272aac7190b1b34a7e965915cbc4ad
Doston <119062575+dosrih@users.noreply.github.com>
2022-12-16 20:42:19 +0500

    Add files via upload

---
+++ hom_c3/Program.cs
@@ -0,0 +1,15 @@
+﻿int lenArray = ReadInt("Enter the length of the array:  ");
+Console.Write("[ ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1, 100);
+    Console.Write(randomArray[i] + ", ");
+}
+Console.Write("]");
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+};
