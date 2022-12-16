c38f6e5b8537818a36c5aea1f6d6625cc4ce4cd1
Doston <119062575+dosrih@users.noreply.github.com>
2022-12-16 20:41:27 +0500

    Add files via upload

---
+++ hom_c2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter a number");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("the sum of all digits in the number is equal to: " + sum);
